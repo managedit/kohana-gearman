648769d0399edf30b9b6f5afa6f371141e3d5b86
Kiall Mac Innes <kiall@managedit.ie>
2010-07-31 16:41:16 +0100

    Adding initial support for background tasks.

--- classes/gearman/client/pecl.php
+++ classes/gearman/client/pecl.php
@@ -21,7 +21,6 @@ class Gearman_Client_Pecl extends Gearman_Client {
 		{
 			$this->client->addServer($server[0], $server[1]);
 		}
-		
 	}
 
 	public function do_task(Gearman_Task $task, $priority = Gearman::PRIORITY_NORMAL)
@@ -73,8 +72,102 @@ class Gearman_Client_Pecl extends Gearman_Client {
 		return $result;
 	}
 
+	public function do_task_background(Gearman_Task $task, $priority = Gearman::PRIORITY_NORMAL)
+	{
+		do
+		{
+			$job_handle = NULL;
+
+			switch ($priority)
+			{
+				case Gearman::PRIORITY_LOW:
+					$job_handle = $this->client->doLowBackground($task->function_name(), $task->workload());
+					break;
+				case Gearman::PRIORITY_NORMAL:
+					$job_handle = $this->client->doBackground($task->function_name(), $task->workload());
+					break;
+				case Gearman::PRIORITY_HIGH:
+					$job_handle = $this->client->doHighBackground($task->function_name(), $task->workload());
+					break;
+				default:
+					throw new Gearman_Client_Exception('Invalid priority specified');
+			}
+
+			if ($this->client->returnCode() != GEARMAN_SUCCESS)
+			{
+				throw new Gearman_Client_Exception();
+			}
+
+			return $job_handle;
+		}
+		while($this->client->returnCode() != GEARMAN_SUCCESS
+			AND $this->client->returnCode() != GEARMAN_WORK_FAIL);
+
+		return $result;
+	}
+
 	public function do_taskset(Gearman_TaskSet $task)
 	{
 		throw new Gearman_Client_Exception('TaskSet\'s are currently un-supported');
 	}
+
+	public function check_status($job_handle)
+	{
+		$status = $this->client->jobStatus($job_handle);
+
+		if ($status[0])
+		{
+			// Job is known
+			if ($status[1])
+			{
+				// Job is still running - return array(numerator, denomintor)
+				return array($status[2], $status[3]);
+			}
+			else
+			{
+				// Job has completed
+				return array(0,0);
+			}
+		}
+		else
+		{
+			// Job is not known to the server.
+			// TODO: Should this throw an exception?
+			// TODO: Check gearman docs for why a job may not be known.
+			return NULL;
+		}
+
+	}
+	
+	public function check_completion($job_handle)
+	{
+		$status = $this->client->jobStatus($job_handle);
+		
+		if ($status[0])
+		{
+			// Job is known
+			if ($status[1])
+			{
+				// Job is still running
+				return FALSE;
+			}
+			else
+			{
+				// Job has completed
+				return TRUE;
+			}
+		}
+		else
+		{
+			// Job is not known to the server.
+			// TODO: Should this throw an exception?
+			// TODO: Should this return FALSE?
+			// TODO: Check gearman docs for why a job may not be known.
+			return TRUE;
+		}
+	}
+
+
+
+
 }
