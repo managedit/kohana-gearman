11b84b62d5180679d5684e459fa98111d8377751
Kiall Mac Innes <kiall@managedit.ie>
2010-09-21 01:39:17 +0100

    BG task support

--- classes/gearman/client/pecl.php
+++ classes/gearman/client/pecl.php
@@ -60,10 +60,10 @@ class Gearman_Client_Pecl extends Gearman_Client {
 					break;
 				case GEARMAN_WORK_STATUS:
 					$this->handle_status($job, $this->client->doStatus());
-				  break;
+					break;
 				case GEARMAN_WORK_DATA:
 					$this->handle_data($job, $result);
-				  break;
+					break;
 			}
 		}
 		while($this->client->returnCode() != GEARMAN_SUCCESS
@@ -72,39 +72,35 @@ class Gearman_Client_Pecl extends Gearman_Client {
 		return $result;
 	}
 
-//	public function do_task_background(Gearman_Task $task, $priority = Gearman::PRIORITY_NORMAL)
-//	{
-//		do
-//		{
-//			$job_handle = NULL;
-//
-//			switch ($priority)
-//			{
-//				case Gearman::PRIORITY_LOW:
-//					$job_handle = $this->client->doLowBackground($task->function_name(), $task->workload());
-//					break;
-//				case Gearman::PRIORITY_NORMAL:
-//					$job_handle = $this->client->doBackground($task->function_name(), $task->workload());
-//					break;
-//				case Gearman::PRIORITY_HIGH:
-//					$job_handle = $this->client->doHighBackground($task->function_name(), $task->workload());
-//					break;
-//				default:
-//					throw new Gearman_Client_Exception('Invalid priority specified');
-//			}
-//
-//			if ($this->client->returnCode() != GEARMAN_SUCCESS)
-//			{
-//				throw new Gearman_Client_Exception();
-//			}
-//
-//			return $job_handle;
-//		}
-//		while($this->client->returnCode() != GEARMAN_SUCCESS
-//			AND $this->client->returnCode() != GEARMAN_WORK_FAIL);
-//
-//		return $result;
-//	}
+	public function run_job_bg(Gearman_Job $job, $priority = Gearman::PRIORITY_NORMAL)
+	{
+		$job_handle = NULL;
+
+		switch ($priority)
+		{
+			case Gearman::PRIORITY_LOW:
+				$job_handle = $this->client->doLowBackground($job->function_name(), $job->workload());
+				break;
+			case Gearman::PRIORITY_NORMAL:
+				$job_handle = $this->client->doBackground($job->function_name(), $job->workload());
+				break;
+			case Gearman::PRIORITY_HIGH:
+				$job_handle = $this->client->doHighBackground($job->function_name(), $job->workload());
+				break;
+			default:
+				throw new Gearman_Client_Exception('Invalid priority specified');
+		}
+
+		switch ($this->client->returnCode())
+		{
+			case GEARMAN_SUCCESS:
+				return $job_handle;
+				break;
+			default:
+				throw new Gearman_Client_Exception('Unknown error');
+		}
+		
+	}
 
 	public function check_status($job_handle)
 	{
